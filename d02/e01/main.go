3f83be1d350e23acc988b10e39e656c25ee86381
Vincent Boisard <v.boisard@catawiki.nl>
2017-12-02 11:47:17 +0100

    D02E01

---
+++ d02/e01/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "os"
+import "bufio"
+import "io"
+import "fmt"
+import "strings"
+import "strconv"
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func readLines(fh *bufio.Reader, c chan string) {
+  for {
+    line, err := fh.ReadString('\n')
+    c <- strings.TrimSuffix(line, "\n")
+    if err == io.EOF {
+      break
+    }
+  }
+  close(c)
+}
+
+func main() {
+  fh, err := os.Open(os.Args[1])
+  check(err)
+  reader := bufio.NewReader(fh)
+  c := make(chan string, 100)
+
+  go readLines(reader, c)
+
+  sum := 0
+
+  for line := range c {
+    min, max := -1, -1
+    for i, raw_cell := range strings.Split(line, "\t") {
+      cell, err := strconv.Atoi(raw_cell)
+      check(err)
+      if i == 0 {
+        min = cell
+        max = cell
+      } else {
+        if cell < min {
+          min = cell
+        }
+        if cell > max {
+          max = cell
+        }
+      }
+    }
+    sum += max - min
+  }
+  fmt.Println(sum)
+}
