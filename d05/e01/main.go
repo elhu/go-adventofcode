4640574e98d7b4613bb9ed5c6228435490cf0740
Vincent Boisard <v.boisard@catawiki.nl>
2017-12-05 08:10:37 +0100

    D05E01

---
+++ d05/e01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "os"
+import "io/ioutil"
+import "strings"
+import "fmt"
+import "strconv"
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func solve(instructions []int) int {
+  steps, position := 0, 0
+  for ; position >= 0 && position < len(instructions); steps++ {
+    move := instructions[position]
+    instructions[position]++
+    position += move
+  }
+  return steps
+}
+
+func main() {
+  data, err := ioutil.ReadFile(os.Args[1]);
+  check(err)
+
+  raw_lines := strings.Split(string(data), "\n")
+  instructions := make([]int, 0, len(raw_lines))
+  for _, line := range raw_lines {
+    instruction, _  := strconv.Atoi(line)
+    instructions = append(instructions, instruction)
+  }
+
+  fmt.Println(solve(instructions))
+}
