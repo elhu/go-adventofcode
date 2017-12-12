96a44afd1b86aac67080f0fe4e0a5bbe8da95895
Vincent Boisard <v.boisard@catawiki.nl>
2017-12-12 12:11:45 +0100

    D12E01

---
+++ d12/e01/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "io"
+  "os"
+  "regexp"
+  "strconv"
+  "strings"
+)
+
+var nodeExp = regexp.MustCompile(`(?P<name>\d+) <-> (?P<neighbours>(\d+)(, \d+)*)`)
+
+type Node struct {
+  name int
+  neighbours []*Node
+}
+
+func NewNode(name int) *Node {
+  n := Node{name: name}
+  n.neighbours = make([]*Node, 0)
+  return &n
+}
+
+func (n *Node) AddEdge(o *Node) {
+  n.neighbours = append(n.neighbours, o)
+  o.neighbours = append(o.neighbours, n)
+}
+
+func (n *Node) GraphSize() int {
+  size := 0
+  visited := make(map[int]struct{})
+  n.Visit(&size, visited)
+
+  return size
+}
+
+func (n *Node) Visit(size *int, visited map[int]struct{}) {
+  _, seen := visited[n.name]
+  if seen {
+    return
+  }
+
+  visited[n.name] = struct{}{}
+  *size++
+  for _, neighbour := range n.neighbours {
+    neighbour.Visit(size, visited)
+  }
+}
+
+func FindOrCreateNode(nodes map[int]*Node, name int) *Node {
+  n := nodes[name]
+  if n == nil {
+    n = NewNode(name)
+    nodes[name] = n
+  }
+  return n
+}
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func readLines(fh *bufio.Reader, c chan string) {
+  for {
+    line, err := fh.ReadString('\n')
+    c <- strings.TrimSuffix(line, "\n")
+    if err == io.EOF {
+      break
+    }
+  }
+  close(c)
+}
+
+func parseLine(line string) (int, []int) {
+  match := nodeExp.FindStringSubmatch(line)
+  name, _ := strconv.Atoi(match[1])
+  neighbours := make([]int, 0)
+  for _, n := range strings.Split(match[2], ", ") {
+    name, _ := strconv.Atoi(n)
+    neighbours = append(neighbours, name)
+  }
+  return name, neighbours
+}
+
+func buildGraph(c chan string) map[int]*Node {
+  nodes := make(map[int]*Node)
+  for line := range c {
+    name, neighbours := parseLine(line)
+    node := FindOrCreateNode(nodes, name)
+    for _, n := range neighbours {
+      neighbour := FindOrCreateNode(nodes, n)
+      node.AddEdge(neighbour)
+    }
+  }
+  return nodes
+}
+
+func main() {
+  fh, err := os.Open(os.Args[1])
+  check(err)
+  defer fh.Close()
+
+  reader := bufio.NewReader(fh)
+
+  c := make(chan string, 100)
+
+  go readLines(reader, c)
+  nodes := buildGraph(c)
+  fmt.Println(nodes[0].GraphSize())
+}
