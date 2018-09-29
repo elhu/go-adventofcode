12ccedef3f0e57a66c719762c7c3a56113570ecc
Vincent Boisard <boisard.v@gmail.com>
2018-09-29 20:00:26 +0200

    2016 D09E02

---
+++ 2016/d09/e02/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type marker struct {
+	strLength    int
+	repeatLength int
+	repeatTimes  int
+}
+
+var markerExp = regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+
+func processMarker(data []byte) marker {
+	match := markerExp.FindStringSubmatch(string(data))
+	repeatLength, _ := strconv.Atoi(match[1])
+	repeatTimes, _ := strconv.Atoi(match[2])
+	return marker{len(match[0]), repeatLength, repeatTimes}
+}
+
+func decompress(data []byte) int {
+	var res int
+	var pos int
+	for pos = 0; pos < len(data); {
+		if data[pos] == '(' {
+			mark := processMarker(data[pos:])
+			pos += mark.strLength
+			subSize := decompress(data[pos : pos+mark.repeatLength])
+			res += mark.repeatTimes * subSize
+			pos += mark.repeatLength
+		} else {
+			res++
+			pos++
+		}
+	}
+	return res
+}
+
+func main() {
+	data, err := ioutil.ReadFile(os.Args[1])
+	check(err)
+	fmt.Println(decompress([]byte(strings.Replace(strings.Trim(string(data), " \n"), " ", "", -1))))
+}
