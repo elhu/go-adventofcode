af7344adcbd64e3b1faa288efea2ddd8fdc5df22
Vincent Boisard <boisard.v@gmail.com>
2018-12-08 11:02:00 +0100

    D08E01

---
+++ 2018/d08/e01/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func sum(numbers []int) int {
+	s := 0
+	for _, i := range numbers {
+		s += i
+	}
+	return s
+}
+
+type node struct {
+	childCount, metadataCount int
+	children                  []node
+	metadata                  []int
+}
+
+func processNode(n node, data []int, pos int) (node, int) {
+	for i := 0; i < n.childCount; i++ {
+		child := node{}
+		child.childCount = data[pos]
+		child.metadataCount = data[pos+1]
+		pos += 2
+		children, newPos := processNode(child, data, pos)
+		n.children = append(n.children, children)
+		pos = newPos
+	}
+	n.metadata = append(n.metadata, data[pos:pos+n.metadataCount]...)
+	pos += n.metadataCount
+	return n, pos
+}
+
+func solve(n node) int {
+	res := sum(n.metadata)
+	for _, c := range n.children {
+		res += solve(c)
+	}
+	return res
+}
+
+func main() {
+	input, err := ioutil.ReadFile(os.Args[1])
+	check(err)
+	input = bytes.TrimSuffix(input, []byte{'\n'})
+	numbers := bytes.Split(input, []byte{' '})
+	rawTree := make([]int, 0, len(numbers))
+	for _, n := range numbers {
+		i, _ := strconv.Atoi(string(n))
+		rawTree = append(rawTree, i)
+	}
+	node := node{childCount: rawTree[0], metadataCount: rawTree[1]}
+	node, _ = processNode(node, rawTree, 2)
+	fmt.Println(solve(node))
+}
