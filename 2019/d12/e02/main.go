b3c5331736ba408a14f1d8e0c85433118f923890
Vincent Boisard <boisard.v@gmail.com>
2019-12-12 12:55:17 +0100

    D12E02

--- 2019/d12/e02/main.go
+++ 2019/d12/e02/main.go
@@ -53,22 +53,12 @@ func addVector(a, b Coords3d) Coords3d {
 	return Coords3d{a.x + b.x, a.y + b.y, a.z + b.z}
 }
 
-func dump(moons []Moon, step int) {
-	fmt.Printf("After %3d steps:\n", step)
-	for _, m := range moons {
-		fmt.Printf("pos=<x=%4d, y=%4d, z=%4d>, vel=<x=%4d, y=%4d, z=%4d>\n", m.pos.x, m.pos.y, m.pos.z, m.vec.x, m.vec.y, m.vec.z)
-	}
-	fmt.Printf("\n")
-}
-
-func solve(moons []Moon) int {
-	seen := make(map[string]struct{})
-	seen[hash(moons)] = struct{}{}
-	for step := 0; ; step++ {
-		if step%100000 == 0 {
-			fmt.Println(step)
-		}
-		// dump(moons, step)
+func getFrequencies(moons []Moon) (int, int, int) {
+	targetX := hashX(moons)
+	targetY := hashY(moons)
+	targetZ := hashZ(moons)
+	freqX, freqY, freqZ := -1, -1, -1
+	for step := 1; ; step++ {
 		gravityDelta := make([]Coords3d, 4)
 		for i, m := range moons {
 			for j := i + 1; j < len(moons); j++ {
@@ -88,37 +78,67 @@ func solve(moons []Moon) int {
 			moons[i].vec = addVector(moons[i].vec, d)
 			moons[i].pos = addVector(moons[i].pos, moons[i].vec)
 		}
-		h := hash(moons)
-		if _, exists := seen[h]; exists {
-			return step
+		if freqX == -1 && hashX(moons) == targetX {
+			freqX = step
 		}
-		seen[h] = struct{}{}
+		if freqY == -1 && hashY(moons) == targetY {
+			freqY = step
+		}
+		if freqZ == -1 && hashZ(moons) == targetZ {
+			freqZ = step
+		}
+		if freqX != -1 && freqY != -1 && freqZ != -1 {
+			return freqX, freqY, freqZ
+		}
 	}
 }
 
-func abs(i int) int {
-	if i < 0 {
-		return -i
+func hashX(ms []Moon) string {
+	res := make([]string, len(ms))
+	for i := 0; i < len(ms); i++ {
+		res[i] = fmt.Sprintf("%d:%d", ms[i].pos.x, ms[i].vec.x)
 	}
-	return i
+	return strings.Join(res, "|")
 }
 
-func hash(moons []Moon) string {
-	data := make([]string, 0, len(moons))
-	for _, moon := range moons {
-		data = append(data, fmt.Sprintf("%d:%d:%d.%d:%d:%d", moon.pos.x, moon.pos.y, moon.pos.z, moon.vec.x, moon.vec.y, moon.vec.z))
+func hashY(ms []Moon) string {
+	res := make([]string, len(ms))
+	for i := 0; i < len(ms); i++ {
+		res[i] = fmt.Sprintf("%d:%d", ms[i].pos.y, ms[i].vec.y)
 	}
-	return strings.Join(data, "\n")
+	return strings.Join(res, "|")
 }
 
-func totalEnergy(moons []Moon) int {
-	res := 0
-	for _, moon := range moons {
-		pot := abs(moon.pos.x) + abs(moon.pos.y) + abs(moon.pos.z)
-		kin := abs(moon.vec.x) + abs(moon.vec.y) + abs(moon.vec.z)
-		res += pot * kin
+func hashZ(ms []Moon) string {
+	res := make([]string, len(ms))
+	for i := 0; i < len(ms); i++ {
+		res[i] = fmt.Sprintf("%d:%d", ms[i].pos.z, ms[i].vec.z)
 	}
-	return res
+	return strings.Join(res, "|")
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func lcm(a, b int, integers ...int) int {
+	result := a * b / gcd(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = lcm(result, integers[i])
+	}
+
+	return result
+}
+
+func solve(moons []Moon) int {
+	xFreq, yFreq, zFreq := getFrequencies(moons)
+	return lcm(xFreq, yFreq, zFreq)
 }
 
 var moonExp = regexp.MustCompile(`<x=(-?\d+), y=(-?\d+), z=(-?\d+)>`)
