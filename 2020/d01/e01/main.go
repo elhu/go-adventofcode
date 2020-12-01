798d2eff8f57ec03ca5b9670fb80f3332ab4c1e5
Vincent Boisard <boisard.v@gmail.com>
2020-12-01 08:47:36 +0100

    Fix stupid mistake

--- 2020/d01/e01/main.go
+++ 2020/d01/e01/main.go
@@ -16,7 +16,7 @@ func check(e error) {
 
 func solve(numbers []int) int {
 	for i := 0; i < len(numbers); i++ {
-		for j := 1; j < len(numbers); j++ {
+		for j := i + 1; j < len(numbers); j++ {
 			if numbers[i]+numbers[j] == 2020 {
 				return numbers[i] * numbers[j]
 			}
--- 2020/d01/e02/main.go
+++ 2020/d01/e02/main.go
@@ -16,8 +16,8 @@ func check(e error) {
 
 func solve(numbers []int) int {
 	for i := 0; i < len(numbers); i++ {
-		for j := 1; j < len(numbers); j++ {
-			for k := 2; k < len(numbers); k++ {
+		for j := i + 1; j < len(numbers); j++ {
+			for k := j + 1; k < len(numbers); k++ {
 				if numbers[i]+numbers[j]+numbers[k] == 2020 {
 					return numbers[i] * numbers[j] * numbers[k]
 				}
