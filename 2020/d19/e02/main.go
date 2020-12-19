1d5a6a6512cfc00c937dc248e5324f54c4173787
Vincent Boisard <boisard.v@gmail.com>
2020-12-19 13:53:11 +0100

    D19E02

--- 2020/d19/e02/main.go
+++ 2020/d19/e02/main.go
@@ -52,7 +52,7 @@ func buildRuleGraph(rulesRef map[string]*Rule, root string) {
 			for _, name := range option {
 				buildRuleGraph(rulesRef, name)
 			}
-			// The input only has 1 or two parts to each branch
+			// The input only has 1-3 parts to each branch
 			if len(option) == 1 {
 				cache = append(cache, rulesRef[option[0]].cache...)
 			} else if len(option) == 2 {
@@ -84,27 +84,49 @@ func parseRules(input []string) map[string]*Rule {
 			rulesRef[parts[0]] = &rule
 		}
 	}
-	buildRuleGraph(rulesRef, "0")
 	return rulesRef
 }
 
-func solve(rule *Rule, messages []string) int {
+func buildExpressions(rulesRef map[string]*Rule) *regexp.Regexp {
+	// 0: 8 11
+	// 8: 42 | 42 8
+	// 11: 42 31 | 42 11 31
+	// compute subgraphs for 31 and 42 (only deps of 8 and 11, which are the only deps of 0)
+	buildRuleGraph(rulesRef, "31")
+	buildRuleGraph(rulesRef, "42")
+	exp31 := fmt.Sprintf("(%s)", strings.Join(rulesRef["31"].cache, "|"))
+	exp42 := fmt.Sprintf("(%s)", strings.Join(rulesRef["42"].cache, "|"))
+	// 8 is just 42 repeated 1-n times
+	exp8 := fmt.Sprintf("(%s)+", exp42)
+	// 11 is 42 repeated n times followed by 31 repeated the same number of time
+	// build a list for each n from 0 to 10 (the messages aren't that long)
+	var expandedExp11 []string
+	for i := 1; i < 10; i++ {
+		expandedExp11 = append(expandedExp11, fmt.Sprintf("%s{%d}%s{%d}", exp42, i, exp31, i))
+	}
+	// 0 is 8 followed by 11
+	exp0 := fmt.Sprintf("^%s(%s)$", exp8, strings.Join(expandedExp11, "|"))
+	return regexp.MustCompile(exp0)
+}
+
+func solve(exp *regexp.Regexp, messages []string) int {
 	res := 0
 	for _, m := range messages {
-		for _, c := range rule.cache {
-			if m == c {
-				res++
-				break
-			}
+		if exp.MatchString(m) {
+			res++
 		}
 	}
 	return res
 }
 
 func main() {
-	data, err := ioutil.ReadFile(os.Args[1])
+	bData, err := ioutil.ReadFile(os.Args[1])
 	check(err)
-	input := strings.Split(strings.TrimRight(string(data), "\n"), "\n\n")
+	data := string(bData)
+	data = strings.Replace(data, "8: 42", "8: 42 | 42 8", 1)
+	data = strings.Replace(data, "11: 42 31", "11: 42 31 | 42 11 31", 1)
+	input := strings.Split(strings.TrimRight(data, "\n"), "\n\n")
 	rules := parseRules(strings.Split(input[0], "\n"))
-	fmt.Println(solve(rules["0"], strings.Split(input[1], "\n")))
+	exp := buildExpressions(rules)
+	fmt.Println(solve(exp, strings.Split(input[1], "\n")))
 }
