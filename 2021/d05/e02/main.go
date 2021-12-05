dfc9d889719de3c61dc453c0ffb7404884c8dcd7
Vincent Boisard <boisard.v@gmail.com>
2021-12-05 09:48:20 +0100

    2021 D05E02

---
+++ 2021/d05/e02/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"adventofcode/utils/coords/coords2d"
+	"adventofcode/utils/files"
+	"fmt"
+	"os"
+)
+
+func toKey(c coords2d.Coords2d) int {
+	// Consider that a 1M offset is enough to avoid collisions
+	return c.Y*1000000 + c.X
+}
+
+func sort(a, b coords2d.Coords2d) (coords2d.Coords2d, coords2d.Coords2d) {
+	if a.X == b.X {
+		if a.Y > b.Y {
+			return b, a
+		}
+		return a, b
+	} else {
+		if a.X > b.X {
+			return b, a
+		}
+		return a, b
+	}
+}
+
+func solve(input []string) int {
+	vents := make(map[int]int)
+	for _, l := range input {
+		var start, end coords2d.Coords2d
+		fmt.Sscanf(l, "%d,%d -> %d,%d", &start.X, &start.Y, &end.X, &end.Y)
+		start, end = sort(start, end)
+		if start.X != end.X && start.Y != end.Y {
+			for i := 0; start.X+i <= end.X; i++ {
+				if start.Y < end.Y {
+					vents[toKey(coords2d.Coords2d{X: start.X + i, Y: start.Y + i})]++
+				} else {
+					vents[toKey(coords2d.Coords2d{X: start.X + i, Y: start.Y - i})]++
+				}
+			}
+		} else {
+			for i := start.X; i <= end.X; i++ {
+				for j := start.Y; j <= end.Y; j++ {
+					vents[toKey(coords2d.Coords2d{X: i, Y: j})]++
+				}
+			}
+		}
+	}
+	res := 0
+	for _, v := range vents {
+		if v > 1 {
+			res++
+		}
+	}
+	return res
+}
+
+func main() {
+	lines := files.ReadLines(os.Args[1])
+	fmt.Println(solve(lines))
+}
