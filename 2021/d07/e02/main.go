a557e1d31a89a014a009abdd94e7c9d532109005
Vincent Boisard <boisard.v@gmail.com>
2021-12-07 08:32:37 +0100

    2021 D07E02

---
+++ 2021/d07/e02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"adventofcode/utils/files"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func atoi(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+func distanceToCost(dist int) int {
+	return (dist * (dist + 1)) / 2
+}
+
+func computeCost(input []int, point int) int {
+	res := 0
+	for _, n := range input {
+		distance := abs(n - point)
+		res += distanceToCost(distance)
+	}
+	return res
+}
+
+func minMax(input []int) (int, int) {
+	min := input[1]
+	max := 0
+	for _, n := range input {
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+	}
+	return min, max
+}
+
+func solve(input []int) int {
+	min, max := minMax(input)
+	prevCost := 99999999999999999
+	var res int
+	for i := min; i <= max; i++ {
+		cost := computeCost(input, i)
+		if cost > prevCost {
+			res = prevCost
+			break
+		}
+		prevCost = cost
+	}
+	return res
+}
+
+func main() {
+	data := files.ReadLinesWithSeparator(os.Args[1], ",")
+	input := make([]int, len(data))
+	for i, d := range data {
+		input[i] = atoi(d)
+	}
+	fmt.Println(solve(input))
+}
