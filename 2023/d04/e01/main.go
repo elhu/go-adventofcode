b492cee751730e8ba320cf3bddd3230174e5f423
Vincent Boisard <boisard.v@gmail.com>
2023-12-04 08:44:53 +0100

    2023 D04

---
+++ 2023/d04/e01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"adventofcode/utils/files"
+	"adventofcode/utils/sets/intset"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func atoi(s string) int {
+	n, err := strconv.Atoi(s)
+	check(err)
+	return n
+}
+
+func parseLine(line string) (*intset.IntSet, *intset.IntSet) {
+	line = strings.Split(line, ": ")[1]
+	parts := strings.Split(line, " | ")
+	left, right := intset.New(), intset.New()
+	for _, num := range strings.Fields(parts[0]) {
+		left.Add(atoi(num))
+	}
+	for _, num := range strings.Fields(parts[1]) {
+		right.Add(atoi(num))
+	}
+	return left, right
+}
+
+func pow(n, p int) int {
+	res := 1
+	for i := 0; i < p; i++ {
+		res *= n
+	}
+	return res
+}
+
+func solve(lines []string) int {
+	var res int
+	for _, line := range lines {
+		left, right := parseLine(line)
+		interLen := left.Intersection(right).Len()
+		if interLen > 0 {
+			res += pow(2, interLen-1)
+		}
+	}
+	return res
+}
+
+func main() {
+	data := strings.TrimRight(string(files.ReadFile(os.Args[1])), "\n")
+	lines := strings.Split(data, "\n")
+	fmt.Println(solve(lines))
+}
---
+++ 2023/d04/e02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"adventofcode/utils/files"
+	"adventofcode/utils/sets/intset"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func atoi(s string) int {
+	n, err := strconv.Atoi(s)
+	check(err)
+	return n
+}
+
+type card struct {
+	id   int
+	wins int
+	memo int
+}
+
+func parseLine(line string) *card {
+	parts := strings.Split(line, ": ")
+	id := atoi(strings.Fields(parts[0])[1])
+	numbers := parts[1]
+	parts = strings.Split(numbers, " | ")
+	left, right := intset.New(), intset.New()
+	for _, num := range strings.Fields(parts[0]) {
+		left.Add(atoi(num))
+	}
+	for _, num := range strings.Fields(parts[1]) {
+		right.Add(atoi(num))
+	}
+	return &card{id: id, wins: left.Intersection(right).Len(), memo: -1}
+}
+
+func computeWins(cards map[int]*card, card *card) int {
+	if card.memo != -1 {
+		return card.memo
+	}
+	res := 1
+	for i := card.id + 1; i <= card.id+card.wins; i++ {
+		res += computeWins(cards, cards[i])
+	}
+	card.memo = res
+	return res
+}
+
+func solve(lines []string) int {
+	cards := make(map[int]*card)
+	for _, line := range lines {
+		card := parseLine(line)
+		cards[card.id] = card
+	}
+	var res int
+	for _, card := range cards {
+		res += computeWins(cards, card)
+	}
+	return res
+}
+
+func main() {
+	data := strings.TrimRight(string(files.ReadFile(os.Args[1])), "\n")
+	lines := strings.Split(data, "\n")
+	fmt.Println(solve(lines))
+}
