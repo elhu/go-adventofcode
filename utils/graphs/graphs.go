540112b802b0a945b4d56b873386b4b7e298b20f
Vincent Boisard <boisard.v@gmail.com>
2024-01-07 19:25:29 +0100

    Implement unweighted graphs

--- utils/graphs/graphs.go
+++ utils/graphs/graphs.go
@@ -1,25 +1,43 @@
 package graphs
 
-import "errors"
+import (
+	"adventofcode/utils/pqueue"
+	"container/heap"
+	"errors"
+)
 
 var (
-	VertexNotFound = errors.New("Vertex not found")
+	EdgeWeightNotSpecified = errors.New("Edge weight not specified")
+	PathNotFound           = errors.New("Path not found")
+	VertexNotFound         = errors.New("Vertex not found")
+	NotImplemented         = errors.New("Not implemented")
 )
 
 type Edge[K comparable, T any] struct {
 	From, To       T
 	FromKey, ToKey K
+	Weight         int
 }
 
 type Graph[K comparable, T any] struct {
 	Edges    map[K](map[K]Edge[K, T])
 	Vertices map[K]T
+	Weighted bool
 }
 
 func NewGraph[K comparable, T any]() *Graph[K, T] {
 	return &Graph[K, T]{
 		Edges:    make(map[K](map[K]Edge[K, T])),
 		Vertices: make(map[K]T),
+		Weighted: false,
+	}
+}
+
+func NewWeightedGraph[K comparable, T any]() *Graph[K, T] {
+	return &Graph[K, T]{
+		Edges:    make(map[K](map[K]Edge[K, T])),
+		Vertices: make(map[K]T),
+		Weighted: true,
 	}
 }
 
@@ -27,7 +45,7 @@ func (g *Graph[K, T]) AddVertex(key K, value T) {
 	g.Vertices[key] = value
 }
 
-func (g *Graph[K, T]) getVertex(k K) (T, error) {
+func (g *Graph[K, T]) GetVertex(k K) (T, error) {
 	v, found := g.Vertices[k]
 	if !found {
 		return v, VertexNotFound
@@ -35,12 +53,16 @@ func (g *Graph[K, T]) getVertex(k K) (T, error) {
 	return v, nil
 }
 
-func (g *Graph[K, T]) AddEdge(from K, to K) error {
-	fromVertex, err := g.getVertex(from)
+func (g *Graph[K, T]) AddEdge(from K, to K, weight ...int) error {
+	if g.Weighted && len(weight) != 1 {
+		return EdgeWeightNotSpecified
+	}
+
+	fromVertex, err := g.GetVertex(from)
 	if err != nil {
 		return err
 	}
-	toVertex, err := g.getVertex(to)
+	toVertex, err := g.GetVertex(to)
 	if err != nil {
 		return err
 	}
@@ -48,16 +70,20 @@ func (g *Graph[K, T]) AddEdge(from K, to K) error {
 	if _, found := g.Edges[from]; !found {
 		g.Edges[from] = make(map[K]Edge[K, T])
 	}
-	g.Edges[from][to] = Edge[K, T]{fromVertex, toVertex, from, to}
+	w := 1
+	if g.Weighted {
+		w = weight[0]
+	}
+	g.Edges[from][to] = Edge[K, T]{fromVertex, toVertex, from, to, w}
 	return nil
 }
 
 func (g *Graph[K, T]) RemoveEdge(from K, to K) error {
-	_, err := g.getVertex(from)
+	_, err := g.GetVertex(from)
 	if err != nil {
 		return err
 	}
-	_, err = g.getVertex(to)
+	_, err = g.GetVertex(to)
 	if err != nil {
 		return err
 	}
@@ -67,6 +93,22 @@ func (g *Graph[K, T]) RemoveEdge(from K, to K) error {
 }
 
 func (g *Graph[K, T]) ShortestPath(from K, to K) ([]K, error) {
+	if g.Weighted {
+		return g.weightedShortestPath(from, to)
+	} else {
+		return g.unWeightedShortestPath(from, to)
+	}
+}
+
+func (g *Graph[K, T]) ShortestDistance(from K, to K) (int, error) {
+	if g.Weighted {
+		return g.weightedShortestDistance(from, to)
+	} else {
+		return -1, NotImplemented
+	}
+}
+
+func (g *Graph[K, T]) unWeightedShortestPath(from K, to K) ([]K, error) {
 	type state struct {
 		current K
 		visited []K
@@ -88,5 +130,69 @@ func (g *Graph[K, T]) ShortestPath(from K, to K) ([]K, error) {
 			}
 		}
 	}
-	return nil, errors.New("No path found")
+	return nil, PathNotFound
+}
+
+func (g *Graph[K, T]) weightedShortestPath(from K, to K) ([]K, error) {
+	const maxInt = 2147483647
+
+	visited := make(map[K]int)
+	prev := make(map[K]K)
+	for k := range g.Vertices {
+		visited[k] = maxInt
+	}
+	visited[from] = 0
+
+	var pq pqueue.PriorityQueue[K]
+	heap.Init(&pq)
+	heap.Push(&pq, &pqueue.Item[K]{Value: from, Priority: 0})
+
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*pqueue.Item[K])
+		if item.Value == to {
+			path := []K{to}
+			for prev[to] != from {
+				path = append(path, prev[to])
+				to = prev[to]
+			}
+			path = append(path, from)
+			return path, nil
+		}
+		for _, edge := range g.Edges[item.Value] {
+			if visited[edge.ToKey] > visited[edge.FromKey]+edge.Weight {
+				visited[edge.ToKey] = visited[edge.FromKey] + edge.Weight
+				prev[edge.ToKey] = edge.FromKey
+				heap.Push(&pq, &pqueue.Item[K]{Value: edge.ToKey, Priority: visited[edge.ToKey]})
+			}
+		}
+	}
+	return nil, PathNotFound
+}
+
+func (g *Graph[K, T]) weightedShortestDistance(from K, to K) (int, error) {
+	const maxInt = 2147483647
+
+	visited := make(map[K]int)
+	for k := range g.Vertices {
+		visited[k] = maxInt
+	}
+	visited[from] = 0
+
+	var pq pqueue.PriorityQueue[K]
+	heap.Init(&pq)
+	heap.Push(&pq, &pqueue.Item[K]{Value: from, Priority: 0})
+
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*pqueue.Item[K])
+		if item.Value == to {
+			return visited[item.Value], nil
+		}
+		for _, edge := range g.Edges[item.Value] {
+			if visited[edge.ToKey] > visited[edge.FromKey]+edge.Weight {
+				visited[edge.ToKey] = visited[edge.FromKey] + edge.Weight
+				heap.Push(&pq, &pqueue.Item[K]{Value: edge.ToKey, Priority: visited[edge.ToKey]})
+			}
+		}
+	}
+	return -1, PathNotFound
 }
---
+++ utils/pqueue/pqueue.go
@@ -0,0 +1,41 @@
+package pqueue
+
+type Item[K comparable] struct {
+	Value    K   // The value of the item; arbitrary.
+	Priority int // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A priorityQueue implements heap.Interface and holds Items.
+type PriorityQueue[K comparable] []*Item[K]
+
+func (pq PriorityQueue[K]) Len() int { return len(pq) }
+
+func (pq PriorityQueue[K]) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].Priority > pq[j].Priority
+}
+
+func (pq PriorityQueue[K]) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue[K]) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item[K])
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue[K]) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
