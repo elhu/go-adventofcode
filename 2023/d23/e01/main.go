492af7e91465f52d44b40c8e881bc935a33bf761
Vincent Boisard <boisard.v@gmail.com>
2024-11-26 21:05:51 +0100

    2023 D23E01 - Refactor usage of pqueue

--- 2023/d23/e01/main.go
+++ 2023/d23/e01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode/utils/coords/coords2d"
 	"adventofcode/utils/files"
+	"adventofcode/utils/pqueue"
 	set "adventofcode/utils/sets"
 	"container/heap"
 	"fmt"
@@ -20,47 +21,12 @@ func pad(lines []string, char string) []string {
 	return padded
 }
 
-type PriorityQueue []*State
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].priority > pq[j].priority
-	// return pq[i].priority < pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x any) {
-	n := len(*pq)
-	item := x.(*State)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
 type State struct {
-	pos      coords2d.Coords2d
-	visited  *set.Set[coords2d.Coords2d]
-	priority int
-	index    int
+	pos     coords2d.Coords2d
+	visited *set.Set[coords2d.Coords2d]
 }
 
 var (
-	zero  = coords2d.Coords2d{X: 0, Y: 0}
 	north = coords2d.Coords2d{X: 0, Y: -1}
 	east  = coords2d.Coords2d{X: 1, Y: 0}
 	south = coords2d.Coords2d{X: 0, Y: 1}
@@ -76,15 +42,16 @@ var vectors = map[byte][]coords2d.Coords2d{
 }
 
 func astar(grid []string, startPos coords2d.Coords2d) int {
-	var pq PriorityQueue
+	var pq pqueue.PriorityQueue[*State]
 	heap.Init(&pq)
 	visited := set.New[coords2d.Coords2d]()
 	visited.Add(startPos)
-	heap.Push(&pq, &State{pos: startPos, visited: visited, priority: 0})
+	heap.Push(&pq, &pqueue.Item[*State]{Value: &State{pos: startPos, visited: visited}, Priority: 0})
 	target := coords2d.Coords2d{X: len(grid[0]) - 3, Y: len(grid) - 2}
 	var candidatePaths []*set.Set[coords2d.Coords2d]
 	for pq.Len() > 0 {
-		curr := heap.Pop(&pq).(*State)
+		item := heap.Pop(&pq).(*pqueue.Item[*State])
+		curr := item.Value
 		curr.visited.Add(curr.pos)
 		if curr.pos == target {
 			candidatePaths = append(candidatePaths, curr.visited)
@@ -93,9 +60,9 @@ func astar(grid []string, startPos coords2d.Coords2d) int {
 		for _, dir := range vectors[grid[curr.pos.Y][curr.pos.X]] {
 			next := coords2d.Add(curr.pos, dir)
 			if !curr.visited.HasMember(next) && grid[next.Y][next.X] != '#' {
-				visited := set.NewFromSlice[coords2d.Coords2d](curr.visited.Members())
+				visited := set.NewFromSlice(curr.visited.Members())
 				visited.Add(next)
-				heap.Push(&pq, &State{pos: next, visited: visited, priority: curr.priority + 1})
+				heap.Push(&pq, &pqueue.Item[*State]{Value: &State{pos: next, visited: visited}, Priority: item.Priority + 1})
 			}
 		}
 	}
